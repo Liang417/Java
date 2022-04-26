0e6c27cbbc0694bb9bcec92f9920e8ad2847781d
Liang <72845312+Liang417@users.noreply.github.com>
2022-04-26 12:58:25 +0800

    4/26 13:00

--- src/temp/ft110101_2.java
+++ src/temp/ft110101_2.java
@@ -7,25 +7,25 @@ public class ft110101_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int sets = sc.nextInt();
+		int sets = sc.nextInt();//幾組測試資料
 		for (int k = 0; k < sets; k++) {
-			String input = sc.nextLine();
-			sc.nextLine();
+			String input = sc.next();
 			int n = sc.nextInt();
 			char[] arr = input.toCharArray();
 			String newStr = "";
 			int mod = n % input.length();
 			if (mod == 0) {
-				System.out.print(input);
+				System.out.println(input);
 			} else {
-				for (int i = input.length() - mod; i < input.length(); i++) {
+				for (int i = mod ; i < input.length(); i++) {
 					newStr += arr[i];
 				}
-				for (int i = 0; i < input.length() - mod; i++) {
+				for (int i = 0; i < mod; i++) {
 					newStr += arr[i];
 				}
 			}
-			System.out.print(newStr);
+			System.out.println(newStr);
 		}
+		sc.close();
 	}
 }
