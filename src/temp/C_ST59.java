5f17a90a0b8499493e63a298967307304d354216
Liang <jwu417@gmail.com>
2022-04-27 08:08:06 +0800

    4/27 08:10

---
+++ src/temp/C_ST59.java
@@ -0,0 +1,39 @@
+package temp;
+/* 題目類型:字串旋轉運算
+ * 題目網址:
+ * https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=3578
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C_ST59 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char[] arr = new char[26];
+		int asciiValue = 97;// 從a開始
+		for (int i = 0; i < arr.length; i++) {// 建立a-z字元陣列
+			arr[i] = (char) asciiValue++;
+		}
+		char[] newArr = Arrays.copyOf(arr, arr.length);
+		for (int i = 0; i < 5; i++) {
+			int index = sc.nextInt();
+			int shift = sc.nextInt();
+			newArr[index] = function(arr, index, shift);
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(newArr[i]);
+		}
+		System.out.println();
+	}
+
+	public static char function(char[] arr, int index, int shift) {
+		int temp = (index + shift) % 26;
+		if (temp < 0) {
+			return arr[26 + (index + shift) % 26];//
+		} else {
+			return arr[temp];
+		}
+	}
+}
--- src/temp/ft110101_2.java
+++ src/temp/ft110101_2.java
@@ -7,7 +7,7 @@ public class ft110101_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int sets = sc.nextInt();//幾組測試資料
+		int sets = sc.nextInt();// 幾組測試資料
 		for (int k = 0; k < sets; k++) {
 			String input = sc.next();
 			int n = sc.nextInt();
@@ -17,14 +17,14 @@ public class ft110101_2 {
 			if (mod == 0) {
 				System.out.println(input);
 			} else {
-				for (int i = mod ; i < input.length(); i++) {
+				for (int i = mod; i < input.length(); i++) {
 					newStr += arr[i];
 				}
 				for (int i = 0; i < mod; i++) {
 					newStr += arr[i];
 				}
+				System.out.println(newStr);
 			}
-			System.out.println(newStr);
 		}
 		sc.close();
 	}
