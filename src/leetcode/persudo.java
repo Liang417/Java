e6d2d8921b302db9b2a10447e931696cd4725886
Liang <72845312+Liang417@users.noreply.github.com>
2022-05-13 12:10:56 +0800

    5/13 1210

--- src/leetcode/persudo.java
+++ src/leetcode/persudo.java
@@ -5,32 +5,42 @@ import java.util.List;
 
 public class persudo {
 	public List<Integer> spiralOrder(int[][] matrix) {
-		int rowUp = 0;
-		int columnLeft = 0;
-		int rowDown = matrix.length - 1;
-		int columnRight = matrix[0].length - 1;
+		int m = matrix.length;
+		int n = matrix[0].length;
+		int up = 0;
+		int left = 0;
+		int down = m - 1;
+		int right = n - 1;
+		int dir = 0;
 		List<Integer> list = new ArrayList<>();
-		while (rowUp <= rowDown && columnLeft <= columnRight) {
-			for (int i = columnLeft; i <= columnRight; i++) {
-				list.add(matrix[rowUp][i]);
-			}
-			rowUp++;
-			for (int i = rowUp; i <= rowDown; i++) {
-				list.add(matrix[i][columnRight]);
-			}
-			columnRight--;
-			for (int i = columnRight; i >= columnLeft; i--) {
-				list.add(matrix[rowDown][i]);
-			}
-			rowDown--;
-
-			if (rowDown >= rowUp) {
-				for (int i = rowDown; i < rowUp; i--) {
-					list.add(matrix[i][columnLeft]);
+		while (up <= down && left <= right) {
+			switch (dir) {
+			case 0:// right
+				for (int i = left; i <= right; i++) {
+					list.add(matrix[up][i]);
 				}
-				columnLeft--;
+				up++;
+				break;
+			case 1:// down
+				for (int i = up; i <= down; i++) {
+					list.add(matrix[i][right]);
+				}
+				right--;
+				break;
+			case 2:// left
+				for (int i = right; i >= left; i--) {
+					list.add(matrix[down][i]);
+				}
+				down--;
+				break;
+			case 3:// up
+				for (int i = down; i >= up; i--) {
+					list.add(matrix[i][left]);
+				}
+				left++;
+				break;
 			}
-			break;
+			dir = (dir + 1) % 4;
 		}
 		return list;
 	}
